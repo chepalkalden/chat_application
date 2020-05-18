ad48ce376fa91b41f44c7555d12d8dfb36d7eadb
Chepal Kalden Sherpa <chepalkalden@gmail.com>
2020-05-18 15:38:43 +0545

    created a post message service

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 var express = require('express') //requiring express
+var bodyParser = require('body-parser') //requiring body parser
 var app = express() //referencing an instance of express on the app variable
 
 // using app.use function to get a prepped for HTML file in other words for serving static contents
 app.use(express.static(__dirname)) //__dirname is used to pass the entire directory
-
+app.use(bodyParser.json()) //this lets bodyParser know that we expect JSON to be coming in with our http request
+app.use(bodyParser.urlencoded({extended: false}))
 var messages = [
     {name: 'Chepal', message: 'Hi'},
     {name: 'Lampard', message: 'Hey, Mate'}
 ]
-app.get('/messages',(req, res)=>{
-    // res.send('Test response') // Testing route   
+
+//Get Message Service Endpoint
+app.get('/messages',(req, res) => {
+    // res.send('Test response') // Testing get request route.  
 
     res.send(messages) // Sending data   
 })
 
+//Post Message Service Endpoint
+app.post('/messages', (req,res) => {
+    console.log(req.body) //testing post request route.
+    messages.push(req.body)//adding the new messages to the messages array.
+    res.sendStatus(200)
+})
 
 /** 
  * to know on which port we are running, we can use a empty callback function and console log the port
