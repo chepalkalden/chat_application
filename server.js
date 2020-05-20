5b3945cc2162cb020a843f974f6405dbb6642049
Chepal Kalden Sherpa <chepalkalden@gmail.com>
2020-05-20 15:08:07 +0545

    configured connection to MongoDB and Added data models and schemas for data insertion

--- server.js
+++ server.js
@@ -10,11 +10,24 @@ var app = express() //referencing an instance of express on the app variable
 var http = require('http').Server(app) //creating a http server and pass our express app in the server.
 var io = require('socket.io') (http) // requiring the Socket.io and passing a reference to our http Server.
 
+/**
+ * For DB Conncetion on MongoDB using mongoClient driver
+ */
+// const MongoClient = require('mongodb').MongoClient;
+var mongoose = require ('mongoose')
 
 // using app.use function to get a prepped for HTML file in other words for serving static contents
 app.use(express.static(__dirname)) //__dirname is used to pass the entire directory
 app.use(bodyParser.json()) //this lets bodyParser know that we expect JSON to be coming in with our http request
 app.use(bodyParser.urlencoded({extended: false}))
+
+//data structure i.e. model and schema  for our message object
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+
 var messages = [
     {name: 'Chepal', message: 'Hi'},
     {name: 'Lampard', message: 'Hey, Mate'}
@@ -32,9 +45,16 @@ app.get('/messages',(req, res) => {
 //Post Message Service Endpoint
 app.post('/messages', (req,res) => {
     // console.log(req.body) //testing post request route.
-    messages.push(req.body)//adding the new messages to the messages array.
-    io.emit('message', req.body) //submiting an event (notification) from the server to all clients notifying them of a new message. Here message is the event name and req.body is the message
-    res.sendStatus(200)
+
+    //message object based on the model to pass into db
+    var message = new Message (req.body)
+    message.save((err) => {
+        if(err)
+            sendStatus(500)//server error
+        messages.push(req.body)//adding the new messages to the messages array.
+        io.emit('message', req.body) //submiting an event (notification) from the server to all clients notifying them of a new message. Here message is the event name and req.body is the message
+        res.sendStatus(200)
+    })
 })
 
 
@@ -43,6 +63,23 @@ app.post('/messages', (req,res) => {
  */
 io.on('connection', (socket) => {
     console.log('user connected')
+})
+
+/**
+ * Connecting to db using MongoClient
+ */
+ 
+const uri = "mongodb+srv://chepal:admin@personalproject-fj3v9.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useUnifiedTopology: true }, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log('mongo db connection', err)
+//   client.close();
+// });
+
+mongoose.connect(uri, { useUnifiedTopology: true , useNewUrlParser: true }, (err) =>{
+  console.log('mongo db connection', err)
 })
 
 
